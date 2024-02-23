0d3d8e0434cc78ca0c954091c999231d2bf52294
Lux <iansseijelly@berkeley.edu>
2024-02-23 12:31:43 -0800

    Casual updates, teddy works with numdisp=45

--- run_cpp.sh
+++ run_cpp.sh
@@ -1,4 +1,5 @@
 python3 src/cpp/util_write.py --left data/cones/right.png --right data/cones/left.png 
+# python3 src/cpp/util_write.py --left data/teddy/im6.png --right data/teddy/im2.png 
 cd src/cpp
 gcc -o stereoBM stereoBM.c
 ./stereoBM

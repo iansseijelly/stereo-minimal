be0dcc8e3c51748b08fb7808b64829a47c8158a6
Lux <iansseijelly@berkeley.edu>
2024-02-23 11:27:36 -0800

    Added attribution to sbm.cpp

--- src/cpp/stereoBM.c
+++ src/cpp/stereoBM.c
@@ -1,3 +1,5 @@
+// This code is adapted from the following source:
+// https://github.com/ankitdhall/stereo-block-matching/blob/master/sbm.cpp
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>

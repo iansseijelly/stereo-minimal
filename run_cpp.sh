53c0341982d13af827f5fa6908079f0b638175dc
Lux <iansseijelly@berkeley.edu>
2024-04-25 23:44:37 -0700

    adding new datasets

--- run_cpp.sh
+++ run_cpp.sh
@@ -1,5 +1,5 @@
-python3 src/cpp/util_write.py --left data/cones/left.png --right data/cones/right.png 
-# python3 src/cpp/util_write.py --left data/teddy/im6.png --right data/teddy/im2.png 
+# python3 src/cpp/util_write.py --left data/tunnel/left.png --right data/tunnel/right.png 
+python3 src/cpp/util_write.py --left data/sewer/left.png --right data/sewer/right.png
 cd src/cpp
 gcc -o stereoBM stereoBM.c
 ./stereoBM
--- run_python.sh
+++ run_python.sh
@@ -1,2 +1,2 @@
-python3 src/python/stereoBM.py --left data/cones/left.png --right data/cones/right.png --num-disparity 64 --block-size 9
-python3 src/python/stereoSGBM.py --left data/cones/left.png --right data/cones/right.png  --num-disparity 64 --block-size 9 --mode SGBM
+python3 src/python/stereoBM.py --left data/sewer/left.png --right data/sewer/right.png --num-disparity 16 --block-size 4
+python3 src/python/stereoSGBM.py --left data/sewer/left.png --right data/sewer/right.png  --num-disparity 16 --block-size 4 --mode SGBM
